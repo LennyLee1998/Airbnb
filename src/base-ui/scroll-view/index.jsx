514b650e585d72dcc83668caf08341214d04740b
Lenny <2484579947@qq.com>
2024-02-17 10:57:04 +0800

    scroll-view  and btn completed

--- src/base-ui/scroll-view/index.jsx
+++ src/base-ui/scroll-view/index.jsx
@@ -1,6 +1,8 @@
 import PropTypes from "prop-types";
 import React, { memo, useEffect, useRef, useState } from "react";
 import { ViewWrapper } from "./style";
+import IconArrowRight from "@/assets/svg/icon-arrow-right";
+import IconArrowLeft from "@/assets/svg/icon-arrow-left";
 
 const ScrollView = memo((props) => {
   const [showLeft, setShowLeft] = useState(false);
@@ -17,33 +19,61 @@ const ScrollView = memo((props) => {
     setShowRight(totalDistance > 0);
   }, [props.children]);
 
-  function rightClickHandle() {
-    const newIndex = posIndex + 1;
+  //
+  function btnClickHandle(isRight = true) {
+    const newIndex = isRight ? posIndex + 1 : posIndex - 1;
     const newEl = scrollContentRef.current.children[newIndex];
     const newOffsetLeft = newEl.offsetLeft;
     console.log(newOffsetLeft);
+    // 两个都为负数是因为translate的值是相对于元素原来的位置进行偏移的
     scrollContentRef.current.style.transform = `translateX(-${newOffsetLeft}px)`;
     setPosIndex(newIndex);
     setShowRight(totalDistanceRef.current > newOffsetLeft);
     setShowLeft(newOffsetLeft > 0);
   }
-  function leftClickHandle() {
-    const newIndex = posIndex - 1;
-    const newEl = scrollContentRef.current.children[newIndex];
-    const newOffsetLeft = newEl.offsetLeft;
-    console.log(newOffsetLeft);
-    scrollContentRef.current.style.transform = `translateX(-${newOffsetLeft}px)`;
-    setPosIndex(newIndex);
-    setShowRight(totalDistanceRef.current > newOffsetLeft);
-    setShowLeft(newOffsetLeft > 0);
-  }
+  // function rightClickHandle() {
+  //   const newIndex = posIndex + 1;
+  //   const newEl = scrollContentRef.current.children[newIndex];
+  //   const newOffsetLeft = newEl.offsetLeft;
+  //   console.log(newOffsetLeft);
+  //   // 两个都为负数是因为translate的值是相对于元素原来的位置进行偏移的
+  //   scrollContentRef.current.style.transform = `translateX(-${newOffsetLeft}px)`;
+  //   setPosIndex(newIndex);
+  //   setShowRight(totalDistanceRef.current > newOffsetLeft);
+  //   setShowLeft(newOffsetLeft > 0);
+  // }
+  // function leftClickHandle() {
+  //   const newIndex = posIndex - 1;
+  //   const newEl = scrollContentRef.current.children[newIndex];
+  //   const newOffsetLeft = newEl.offsetLeft;
+  //   console.log(newOffsetLeft);
+  //   scrollContentRef.current.style.transform = `translateX(-${newOffsetLeft}px)`;
+  //   setPosIndex(newIndex);
+  //   setShowRight(totalDistanceRef.current > newOffsetLeft);
+  //   setShowLeft(newOffsetLeft > 0);
+  // }
   return (
     <ViewWrapper>
-      {showLeft && <button onClick={leftClickHandle}>左边按钮</button>}
-      {showRight && <button onClick={rightClickHandle}>右边按钮</button>}
+      <div className="wrapper">
+        {showLeft && (
+          <div
+            className="btn left"
+            onClick={(e) => {
+              btnClickHandle(false);
+            }}
+          >
+            <IconArrowLeft />
+          </div>
+        )}
+        {showRight && (
+          <div className="btn right" onClick={btnClickHandle}>
+            <IconArrowRight />
+          </div>
+        )}
 
-      <div className="scroll-content" ref={scrollContentRef}>
-        {props.children}
+        <div className="scroll-content" ref={scrollContentRef}>
+          {props.children}
+        </div>
       </div>
     </ViewWrapper>
   );
--- src/base-ui/scroll-view/style.js
+++ src/base-ui/scroll-view/style.js
@@ -1,9 +1,38 @@
 import styled from "styled-components";
 
 export const ViewWrapper = styled.div`
-  overflow: hidden;
+  position: relative;
+  .wrapper {
+    overflow: hidden;
+  }
+  .btn {
+    position: absolute;
+    z-index: 99;
+    top: 50%;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 28px;
+    height: 28px;
+    border-radius: 14px;
+    /* background-color: red; */
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.18);
+    cursor: pointer;
+    background-color: #fff;
+
+    &.left {
+      left: 0;
+      transform: translate(-50%, -50%);
+    }
+    &.right {
+      right: 0;
+      transform: translate(50%, -50%);
+    }
+  }
   .scroll-content {
     position: relative;
+    z-index: 9;
     display: flex;
     padding: 8px 0;
 
